3e6fd7ce6a46c20e05fc04e571b717917f67596b
Szymon Fortuna <131533540+szymonf123@users.noreply.github.com>
2025-04-11 11:55:46 +0200

    Add new views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.get('/actual-tenders', function(req, res, next) {
+  res.render('actual-tenders');
+});
+
+router.get('/cancelled-tenders', function(req, res, next) {
+  res.render('cancelled-tenders');
+});
+
+router.get('/add-tender', function(req, res, next) {
+  res.render('add-tender');
 });
 
 module.exports = router;
