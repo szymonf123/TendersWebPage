69b61931afc53712a0799296c424fbbc7a5e5fa3
Szymon Fortuna <131533540+szymonf123@users.noreply.github.com>
2025-04-23 20:29:12 +0200

    Fix sending offers to unactive tenders

--- public/javascripts/controllers/OfferController.js
+++ public/javascripts/controllers/OfferController.js
@@ -17,7 +17,6 @@ class OfferController {
         }
 
         let sql = `INSERT INTO offers (tender_id, bidder_name, value, submission_datetime) VALUES (${offer.tenderId}, "${offer.bidderName}", ${offer.price}, NOW())`;
-        console.log(sql);
         return new Promise((resolve, reject) => {
             db.query(sql, (err, tender) => {
                 if (err) return reject(err);
--- public/javascripts/controllers/TenderController.js
+++ public/javascripts/controllers/TenderController.js
@@ -20,7 +20,7 @@ class TenderController {
     }
 
     static getTenderById(db, selected_id) {
-        const sql = "SELECT *, DATE_FORMAT(start_date, '%d.%m.%Y') AS f_start_date, DATE_FORMAT(end_date, '%d.%m.%Y') AS f_end_date, CONCAT(start_date, ' ', start_time) <= NOW() AS is_active FROM tenders WHERE id = " + selected_id;
+        const sql = "SELECT *, DATE_FORMAT(start_date, '%d.%m.%Y') AS f_start_date, DATE_FORMAT(end_date, '%d.%m.%Y') AS f_end_date, CONCAT(start_date, ' ', start_time) <= NOW() AND CONCAT(end_date, ' ', end_time) > NOW()AS is_active FROM tenders WHERE id = " + selected_id;
         return new Promise((resolve, reject) => {
             db.query(sql, (err, data) => {
                 if (err) return reject(err);
